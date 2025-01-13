76ee733d9314b0fa545c3b0539b9ab11e66333c3
Teame-sy <transwiki@proton.me>
2025-01-13 21:28:32 +0900

    Update source.c
    가독성 개선

--- source.c
+++ source.c
@@ -2,8 +2,8 @@
 #define _CRT_SECURE_NO_WARNINGS //for vs complier
 
 // made by github.com/teame-sy
-// 미완성- 주석 처리
-/*
+// 고칠 거나 오류는 이슈로. PR도 좋음
+
 int plus(int a, int b)
 {
 	int temp;
@@ -43,25 +43,32 @@ double divide(int g, int h)
 void scanf_forinput(void)
 {
 	printf("다음 중 하고 싶은 것을 고르세요.\n");
+	
 	printf("1. 덧셈, 2. 뺄셈 3. 곱셈 4. 나눗셈\n");
+	
 	printf("첫 번째 숫자 입력> ");
 
 	int firstnum;
+	
 	int secondnum;
 
-	
 	scanf("", &firstnum);
+	
 	printf("\n 두 번째 숫자 입력> ");
 
 	scanf("", &secondnum);
+	
 	int nunbr;
+	
 	printf("위 숫자 (사칙 연산 1-4번)중 할 것을 고르세요:");
+	
 	scanf("", &nunbr);
+	
 	int tem;
+	
 	switch(nunbr)
 		case 1:
 			tem = plus(firstnum, secondnum);
-
    		case 2:
      			tem = minus(firstnum, secondnum);
 		case 3:
@@ -70,10 +77,19 @@ void scanf_forinput(void)
        			tem = divide(firstnum, secondnum);
 
 
-    	printf("결과는 
+    	printf("\n 결과는 %d 입니다.", tem)
+
+
+      	// 끝
 	
 
 
 }
 
-*/
+int main(void)
+{
+	scanf_forinput();
+
+}
+
+
